afff53c834b1f0039fddcbc13a69cf53003ddca1
rmtrigo <rmontrigo@gmail.com>
2015-06-03 21:36:04 -0700

    Commit 7: Completed caesar cipher ruby problem

---
+++ ruby_programming/ruby_building_blocks/caesar_cipher.rb
@@ -0,0 +1,12 @@
+def encrypt(msg, key)
+	alphabet = ('a'..'z').to_a
+	encrypted = Hash[alphabet.zip(alphabet.rotate(key.to_i))]
+	msg.chars.map { |c| encrypted.fetch(c, " ") }
+end
+
+print "Enter your message: "
+msg = gets.chomp
+print "Enter an encryption key: "
+key = gets.chomp
+
+puts "Your encrypted message: " << encrypt(msg, key).join
